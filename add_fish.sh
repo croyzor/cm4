76e0b6ec7cdc6e6b6eb36eec3dd2434da7859a6d
ross <ross.mc56@gmail.com>
2015-03-02 00:47:48 +0000

    added fish1

--- add_fish.sh
+++ add_fish.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /home/ross/git/cm4
-auth='find /tmp -user $LOGNAME -type s -name "*agent*" -print 2>/dev/null'
-SSH_AUTH_SOCK=$auth
-export SSH_AUTH_SOCK
-FISH_NUMBER="$(/usr/bin/awk 'END {print $NF}' Fish.sh | /usr/bin/sed 's/[^0-9]//g')"
 FISH_NUMBER=$((FISH_NUMBER+1))
 FISH_STR="fish$FISH_NUMBER"
 /usr/bin/echo "echo $FISH_STR" >> Fish.sh
