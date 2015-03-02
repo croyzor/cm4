c084c0f5be9ec94744aafdbc24258115622a59b8
ross <ross.mc56@gmail.com>
2015-03-02 00:34:02 +0000

    added fish126

--- add_fish.sh
+++ add_fish.sh
@@ -3,7 +3,7 @@ cd /home/ross/git/cm4
 FISH_NUMBER="$(/usr/bin/awk 'END {print $NF}' Fish.sh | /usr/bin/sed 's/[^0-9]//g')"
 FISH_NUMBER=$((FISH_NUMBER+1))
 FISH_STR="fish$FISH_NUMBER"
-/bin/echo "echo $FISH_STR" >> Fish.sh
+/usr/bin/echo "echo $FISH_STR" >> Fish.sh
 /usr/bin/git add .
 /usr/bin/git commit  -m "added $FISH_STR"
 /usr/bin/git push origin master 
